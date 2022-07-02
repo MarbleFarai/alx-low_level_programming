04d82cc3be93263cf8646a23f2de9440d270a032
MarbleFarai <marblefaraii@gmail.com>
2022-07-01 20:59:29 -0700

    4 first attempt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+*main -> assign a random numner to the variable each time it is executed
+*and print the last digit of the number stored in the variable n
+*Return : always 0
+*/
+int main(void)
+{
+	int ch;
+	for(ch=97;ch=127;ch++)
+	{
+		putchar(ch);
+	putchar("\n");
+}
