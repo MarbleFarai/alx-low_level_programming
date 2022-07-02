a17dad565715d5ad70fda4e3359fb9be32622d0a
MarbleFarai <marblefaraii@gmail.com>
2022-07-01 20:03:23 -0700

    2 attempt

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include <stdlib.>
+#include <stdio.>
 #include <time.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
 *main -> assign a random number to the variable n each time it is executed and prints out
