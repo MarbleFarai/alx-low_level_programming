7d8be937d56021ace7ea4d88772e34094714d0ce
MarbleFarai <marblefaraii@gmail.com>
2022-07-04 18:02:20 -0700

    hot fix 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * - Dora Korpar, 2015-10-19",
  * followed byt a new line, tothe standard error.
  */
-int main(void)//
+int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
